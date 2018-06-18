87c2b3ec557ba78ad317988b3df336e931566627
迷失的月亮 <luecsc@gmail.com>
2018-06-18 22:15:06 +0800

    repository init

---
+++ src/main/java/cn/moondev/App.java
@@ -0,0 +1,13 @@
+package cn.moondev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
